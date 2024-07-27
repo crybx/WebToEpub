7707d9de50905956ede0850a54e6223be17fe963
crybx <crybx@users.noreply.github.com>
2024-07-27 11:31:18 -0400

    Remove junk from chrysanthemumgarden.com

--- plugin/js/parsers/ChrysanthemumgardenParser.js
+++ plugin/js/parsers/ChrysanthemumgardenParser.js
@@ -2,7 +2,7 @@
 
 parserFactory.register("chrysanthemumgarden.com", () => new ChrysanthemumgardenParser());
 
-class ChrysanthemumgardenParser extends WordpressBaseParser{
+class ChrysanthemumgardenParser extends WordpressBaseParser {
     constructor() {
         super();
     }
@@ -16,10 +16,33 @@ class ChrysanthemumgardenParser extends WordpressBaseParser{
     customRawDomToContentStep(chapter, content) {
         if (!this.userPreferences.removeAuthorNotes.value) {
             let notes = [...chapter.rawDom.querySelectorAll("div.tooltip-container")];
-            for(let n of notes) {
+            for (let n of notes) {
                 content.appendChild(n);
             }
         }
+
+        let nodes = content.querySelectorAll(".jum");
+        for (let node of nodes) {
+            this.dejumble(node);
+        }
+
+        // get all elements where style contains height of 1px and remove them
+        let onePxElements = content.querySelectorAll("[style*='height:1px']");
+        util.removeElements(onePxElements);
+    }
+
+    dejumble(node) {
+        const alphab = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const cypher = 'jymvfoutlpxiwcbqdgraenkzshCDJGSMXLPABOZRYUHEVKFNQWTI';
+        let sArray = node.textContent.split("");
+        for (let i = 0; i < sArray.length; i++) {
+            let index = cypher.indexOf(sArray[i]);
+            if (index !== -1) {
+                sArray[i] = alphab[index];
+            }
+        }
+        node.textContent = sArray.join("");
+        node.classList.remove("jum");
     }
 
     async fetchChapter(url) {
